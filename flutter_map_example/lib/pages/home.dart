1496e9b3c3580c7ae2291d6886636dd813d1b46b
Sir Mammingtonham <ethanj217@gmail.com>
2020-02-04 14:23:39 -0500

    add example app for leaflet usage

---
+++ flutter_map_example/lib/pages/home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+
+import '../widgets/drawer.dart';
+
+class HomePage extends StatelessWidget {
+  static const String route = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(51.5, -0.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(53.3498, -6.2603),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.green,
+            key: ObjectKey(Colors.green),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(48.8566, 2.3522),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.purple,
+            key: ObjectKey(Colors.purple),
+          ),
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      drawer: buildDrawer(context, route),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Text('This is a map that is showing (51.5, -0.9).'),
+            ),
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c'],
+                    // For example purposes. It is recommended to use
+                    // TileProvider with a caching and retry strategy, like
+                    // NetworkTileProvider or CachedNetworkTileProvider
+                    tileProvider: NonCachingNetworkTileProvider(),
+                  ),
+                  MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
