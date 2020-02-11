e14a179351a5b11d2c8d9ee3290ec8d359855ec6
BoyuChen118 <alexboyuchen@gmail.com>
2020-02-11 17:41:38 -0500

    added login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,217 @@
+ import 'package:flutter/material.dart';
+// import 'dart:ui' as ui;
+// import 'package:flutter/services.dart' show rootBundle;
+// import 'dart:async';
+// import 'dart:typed_data';
+
+ 
+
+ class MyApp extends StatelessWidget {
+      // This widget is the root of your application.
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          title: 'Flutter login UI',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage(title: 'Smartrider Login'),
+        );
+      }
+    }
+
+ class MyHomePage extends StatefulWidget {
+      MyHomePage({Key key, this.title}) : super(key: key);
+      final String title;
+      @override
+      _MyHomePageState createState() => _MyHomePageState();
+    }
+
+    class _MyHomePageState extends State<MyHomePage> {
+      TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+      @override
+      Widget build(BuildContext context) {
+
+        final emailField = TextField(
+          obscureText: false,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Username",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+        final passwordField = TextField(
+          obscureText: true,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Password",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+        final loginButon = Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.green,
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () {},
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+          ),
+        );
+        final signupButton = Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.lightBlue,
+          child: MaterialButton(
+           minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () {},
+            child: Text("Signup",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+          ),
+        );
+
+        
+
+        return Scaffold(
+          body: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 135.0,
+                      child: Image.asset(
+                        "assets/logo.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    signupButton,
+                    SizedBox(
+                      height:  10.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+    }
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: new ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: new MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => new _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   ui.Image image;
+//   bool isImageloaded = false;
+//   void initState() {
+//     super.initState();
+//     init();
+//   }
+
+//   Future <Null> init() async {
+//     final ByteData data = await rootBundle.load('images/lake.jpg');
+//     image = await loadImage(new Uint8List.view(data.buffer));
+//   }
+
+//   Future<ui.Image> loadImage(List<int> img) async {
+//     final Completer<ui.Image> completer = new Completer();
+//     ui.decodeImageFromList(img, (ui.Image img) {
+//       setState(() {
+//         isImageloaded = true;
+//       });
+//       return completer.complete(img);
+//     });
+//     return completer.future;
+//   }
+
+//   Widget _buildImage() {
+//     if (this.isImageloaded) {
+//       return new CustomPaint(
+//           painter: new ImageEditor(image: image),
+//         );
+//     } else {
+//       return new Center(child: new Text('loading'));
+//     }
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text(widget.title),
+//       ),
+//       body: new Container(
+//         child: _buildImage(),
+//       )
+//     );
+//   }
+// }
+
+// class ImageEditor extends CustomPainter {
+
+
+//   ImageEditor({
+//     this.image,
+//   });
+
+//   ui.Image image;
+
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     //ByteData data = image.toByteData();
+//     canvas.drawImage(image, new Offset(0.0, 0.0), new Paint());
+//   }
+
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) {
+//     return false;
+//   }
+
+// }
+
