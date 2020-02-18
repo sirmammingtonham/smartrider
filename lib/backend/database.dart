4ebde874e0e2d20e37c28d1db5197427db1b583b
Yaw Amoa-Asare <theyawguy@gmail.com>
2020-02-17 22:38:23 -0500

    made minor changes to the database structure

--- lib/backend/database.dart
+++ lib/backend/database.dart
@@ -1,15 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/* Contains all the functions for handling the database */
+
 class DatabaseService {
-  final String identifier;
+  //unique user id, will be generated from authorization class after a user successfully logs in
+  final String usid;
+  //reference to the user collection in the database
   final CollectionReference _userCollection = Firestore.instance.collection('users');
 
-  DatabaseService({this.identifier});
+  DatabaseService({this.usid});
 
-  Future updateUserData(String name, String rin) async {
-    return await _userCollection.document(identifier).setData({
+  Future updateUserData(String name, String rin, String userType) async {
+    //Updates the user data for the user corresponding to the usid
+    return await _userCollection.document(usid).setData({
       'name': name,
-      'rin': rin
+      'rin': rin,
+      'userType': userType
     });
   }
+
+  //receive data function(s) go here
+
 }
