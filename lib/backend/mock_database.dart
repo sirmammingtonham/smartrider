1c45f70bc07f5b7987ebbd5ec6a4fa6b80a833ef
Yaw Amoa-Asare <theyawguy@gmail.com>
2020-03-03 15:23:15 -0500

    Got database tests somewhat working

--- lib/backend/mock_database.dart
+++ lib/backend/mock_database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mock_cloud_firestore/mock_cloud_firestore.dart';
 import 'package:mock_cloud_firestore/mock_types.dart';
 import 'package:smartrider/backend/user_list.dart';
 import 'package:smartrider/backend/user.dart';
@@ -10,14 +11,24 @@ class DatabaseService {
   //unique user id, will be generated from authorization class after a user successfully logs in
   final String usid;
   //reference to the user collection in the database
-
-  final CollectionReference _userCollection = MockFirestoreInstance().collection('users');
+  static final  String src = r"""
+{
+  "users": {
+    "1": {
+      "Rin": "1",
+      "id": "1",
+      "name": "1"
+    }
+  }
+}
+  """;
+  final CollectionReference _userCollection = MockCloudFirestore(src).collection('users');
 
   DatabaseService({this.usid});
 
   Future updateUserData(String name, String userType, {String rin="0"}) async {
     //Updates the user data for the user corresponding to the usid
-    return await _userCollection.document(usid).setData({
+    return await _userCollection.document("1").setData({
       'name': name,
       'userType': userType,
       'rin': rin,
@@ -45,4 +56,22 @@ class DatabaseService {
     return _userCollection.snapshots().map(_userListFromSnapshot);
   }
 
+
+
+  void debug() {
+    print("debugging");
+    print(usid);
+    Future<DocumentSnapshot> userRef =  _userCollection.document("1").get();
+    userRef.then( (DocumentSnapshot ds) {
+      print(ds.data);
+    });
+    /*
+    _userCollection.where("topic", isEqualTo: "flutter").snapshots().listen(
+        (data) =>
+            data.documents.forEach( (doc) => print(doc["title"])));
+
+     */
+  }
+
 }
+
