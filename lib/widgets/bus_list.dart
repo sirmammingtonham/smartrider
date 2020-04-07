bdc08ce0fac889c2c2516b5501b6d5f9a3e08052
sirmammingtonham <ethanj217@gmail.com>
2020-04-07 14:05:38 -0700

    updated bus schedule so it doesn't show wrong times

--- lib/widgets/bus_list.dart
+++ lib/widgets/bus_list.dart
@@ -47,7 +47,7 @@ AutomaticKeepAliveClientMixin<BusList>
           controller: _tabController,
         ),
         Container(
-          height: MediaQuery.of(context).size.height * 0.74,
+          height: MediaQuery.of(context).size.height * 0.7,
           child: TabBarView(
             controller: _tabController,
             children: <Widget>[
@@ -71,7 +71,7 @@ Widget busList(int idx, ItemScrollController _scrollController, Function _contai
     itemBuilder: (context, index) {
       var curStopList = busStopLists[idx];
       var curTimeList = busTimeLists[idx];
-      if (!_containsFilter(curStopList, curTimeList, index)) {
+      if (!_containsFilter(curStopList, curTimeList, index) || curTimeList[index] == "- - - -") {
         return null;
       }
       return Card(
