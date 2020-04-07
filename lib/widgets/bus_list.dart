a7648fbd263985f23a13fc855955e5b79df72a19
sirmammingtonham <ethanj217@gmail.com>
2020-04-07 13:09:07 -0700

    updated switching

---
+++ lib/widgets/bus_list.dart
@@ -0,0 +1,88 @@
+// ui dependencies
+import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+// loading custom widgets and data
+import 'package:smartrider/util/schedule_data.dart';
+
+class BusList extends StatefulWidget {
+  final List<ItemScrollController> scrollControllers;
+  final Function containsFilter;
+  BusList({Key key, this.scrollControllers, this.containsFilter}) : super(key: key);
+  @override
+  BusListState createState() => BusListState();
+}
+  
+class BusListState extends State<BusList> with SingleTickerProviderStateMixin,
+AutomaticKeepAliveClientMixin<BusList> 
+{
+  final List<Widget> busTabs = [
+    Tab(text: 'Route 87'),
+    Tab(text: 'Route 286'),
+    Tab(text: 'Route 289'),
+  ];
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: busTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget> [
+        TabBar(
+          tabs: busTabs,
+          // unselectedLabelColor: Colors.white.withOpacity(0.3),
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.black,
+          controller: _tabController,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.74,
+          child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              busList(0, this.widget.scrollControllers[0], this.widget.containsFilter),
+              busList(1, this.widget.scrollControllers[1], this.widget.containsFilter),
+              busList(2, this.widget.scrollControllers[2], this.widget.containsFilter),
+            ],
+          ),
+        )
+      ]
+    );
+  }
+  @override
+  bool get wantKeepAlive => true;
+}
+
+Widget busList(int idx, ItemScrollController _scrollController, Function _containsFilter) {
+  return ScrollablePositionedList.builder(
+    itemCount: busTimeLists[idx].length,
+    itemScrollController: _scrollController,
+    itemBuilder: (context, index) {
+      var curStopList = busStopLists[idx];
+      var curTimeList = busTimeLists[idx];
+      if (!_containsFilter(curStopList, curTimeList, index)) {
+        return null;
+      }
+      return Card(
+        child: ListTile(
+          leading: Icon(Icons.directions_bus),
+          title: Text(curStopList[index%curStopList.length]),
+          subtitle: Text(curTimeList[index]),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () {},
+        ),
+      );
+    },
+  );
+}
