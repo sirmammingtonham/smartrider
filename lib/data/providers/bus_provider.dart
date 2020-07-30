c440cd9125bd77efe1801d56861c51324f75726c
Julian L <jthemage101@gmail.com>
2020-07-30 15:16:04 -0400

    Added bus_bloc template structure

---
+++ lib/data/providers/bus_provider.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+import '../models/bus/bus_advisory.dart';
+import '../models/bus/bus_gtfs.dart';
+import '../models/bus/bus_updates.dart';
+import '../models/bus/bus_vehicles.dart';
+
