c18ca262d59068b0fa007986c554407cfb7c3e81
Julian L <jthemage101@gmail.com>
2020-10-02 17:24:15 -0400

    Moving bus templates to dev branch whoops

---
+++ lib/data/models/bus/bus_routes.dart
@@ -0,0 +1,25 @@
+class BusRoutes {
+  String routeId;
+  String agencyId;
+  String routeShortName;
+  String routeLongName;
+  String routeDesc;
+  String routeType;
+  String routeUrl;
+  String routeColor;
+  String routeTextColor;
+  String routeSortOrder;
+
+  BusRoutes({
+    this.routeId,
+    this.agencyId,
+    this.routeShortName,
+    this.routeLongName,
+    this.routeDesc,
+    this.routeType,
+    this.routeUrl,
+    this.routeColor,
+    this.routeTextColor,
+    this.routeSortOrder
+  });
+}
