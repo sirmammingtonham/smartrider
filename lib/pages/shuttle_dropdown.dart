757ea7b8c89f19bb420a3037647ac38eed192bca
chris32101 <48632502+chris32101@users.noreply.github.com>
2020-10-02 17:02:36 -0400

    Update shuttle_dropdown.dart

---
+++ lib/pages/shuttle_dropdown.dart
@@ -0,0 +1,11 @@
+// ui dependencies
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// loading custom widgets and data
+import 'package:smartrider/util/data.dart';
+import 'package:smartrider/widgets/filter_dialog.dart';
+import 'package:smartrider/widgets/shuttle_list.dart';
+import 'package:smartrider/widgets/bus_list.dart';
+import 'package:smartrider/widgets/map_ui.dart';
