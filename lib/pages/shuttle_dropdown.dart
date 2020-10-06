413340b05cc0bc618f9226facd54cd7a255449ea
chris32101 <48632502+chris32101@users.noreply.github.com>
2020-10-06 16:19:34 -0400

    Update to Shuttle_Dropdown
    This update includes comments for the people who are still confused about each function.

--- lib/pages/shuttle_dropdown.dart
+++ lib/pages/shuttle_dropdown.dart
@@ -28,6 +28,40 @@ import 'package:smartrider/widgets/map_ui.dart';
 import 'package:smartrider/widgets/search_bar.dart';
 import 'package:smartrider/pages/schedule.dart';
 
+/*
+-==-=--=-=--==-=-=-=-=-=-=-=-=
+LOOK HERE LOOK HERE LOOK HERE
+-=-=-=-=-=-=-=-=-=-=--=--=---=
+
+For people in the UI improvement team, we are trying to implement this
+panel system into our current schedule slider.
+To better understand this look at home.dart under pages and change line 93 
+from "panel: test())," to "panel: main()),". And rerun your andriod emulator
+to see what is the current schuedle UI.
+Looking at the current scheduele UI we have a top section that has each individual
+shuttle and bus. Clicking on this allows us to see each shuttles stop. Right now 
+all we have is the bus's stops looping over and over again in a list.
+We are trying to change this to a panel system. Where each stop under a given bus has 
+a panel listing out the times when the bus should be at said stop.
+
+To implement we first should work to understand flutter and this panel system. Then
+we should work to implement this into the schedule.dart file.
+
+Remember that this panel systme should be sub system under each shuttle at the top.
+
+NOTE: Remember to switch back to the testing file in the home.dart file.
+*/
+
+class test extends StatefulWidget {
+  test({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/*The class item is each panel or item and what information is inside of it
+headValue is in the example Panel #. expandedValue is only seen when a penel
+is expanded, this can be used as a description for each shuttle pr stop*/
 class Item {
   Item({
     this.expandedValue,
@@ -40,20 +74,17 @@ class Item {
   bool isExpanded;
 }
 
+/*Creates each item in this function*/
 List<Item> generateItems(int numberOfItems) {
   return List.generate(numberOfItems, (int index) {
     return Item(
+      //Each stop should be the headerValue
       headerValue: 'Panel $index',
+
+      //Each stop descrition should be expandedValue
       expandedValue: 'This is item number $index',
     );
   });
-}
-
-class test extends StatefulWidget {
-  test({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<test> {
@@ -82,9 +113,15 @@ class _MyStatefulWidgetState extends State<test> {
               title: Text(item.headerValue),
             );
           },
+
+          /*This is all the information when we expand the item*/
           body: ListTile(
               title: Text(item.expandedValue),
-              subtitle: Text('To delete this panel, tap the trash can icon'),
+              subtitle: Text('Delete icon'),
+
+              /*Maybe we can add a report function here instead a delete function
+              the report function allows us to create a real time tracker for 
+              the bus*/
               trailing: Icon(Icons.delete),
               onTap: () {
                 setState(() {
