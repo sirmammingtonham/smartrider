bbce8277655db82082d7ddc490650442ad0ef9d8
sirmammingtonham <ethanj217@gmail.com>
2020-10-06 14:09:42 -0700

    cleanup

--- lib/blocs/bus/bus_bloc.dart
+++ lib/blocs/bus/bus_bloc.dart
@@ -4,9 +4,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // model imports
 import 'package:smartrider/data/models/bus/bus_advisory.dart';
-import 'package:smartrider/data/models/bus/bus_gtfs.dart';
-import 'package:smartrider/data/models/bus/bus_updates.dart';
-import 'package:smartrider/data/models/bus/bus_vehicles.dart';
 
 // repository imports
 import 'package:smartrider/data/repository/bus_repository.dart';
