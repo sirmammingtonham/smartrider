3754444d8a5748c7f401d1fb607158476d5a5688
unknown <alexboyuchen@gmail.com>
2020-10-26 19:02:20 -0700

    schedule toggle fixed and integrated with schedule bloc

--- lib/blocs/schedule/schedule_event.dart
+++ lib/blocs/schedule/schedule_event.dart
@@ -9,10 +9,25 @@ class ScheduleInitEvent extends ScheduleEvent{
   List<Object> get props => [];
 }
 class ScheduleTimelineEvent extends ScheduleEvent{
+
+  const ScheduleTimelineEvent();
   @override
   List<Object> get props => [];
 }
 class ScheduleTableEvent extends ScheduleEvent{
+
+  const ScheduleTableEvent();
+  @override
+  List<Object> get props => [];
+}
+class ScheduleChangeEvent extends ScheduleEvent{
+  
+  @override
+  List<Object> get props => [];
+}
+class ScheduleTransitionEvent extends ScheduleEvent{
+  final ScheduleState currentstate;
+  const ScheduleTransitionEvent({@required this.currentstate});
   @override
   List<Object> get props => [];
 }
--- lib/blocs/schedule/schedule_state.dart
+++ lib/blocs/schedule/schedule_state.dart
@@ -8,10 +8,29 @@ class ScheduleInitialState extends ScheduleState{
   List<Object> get props => [];
 }
 class ScheduleTimelineState extends ScheduleState{
+  final bool isShuttle;
+  const ScheduleTimelineState({@required this.isShuttle});
   @override
   List<Object> get props => [];
 }
 class ScheduleTableState extends ScheduleState{
+  final bool isShuttle;
+  const ScheduleTableState({@required this.isShuttle});
+  @override
+  List<Object> get props => [];
+}
+
+class ScheduleChangeState extends ScheduleState{
+  final bool isShuttle;
+  const ScheduleChangeState({@required this.isShuttle});
+  @override
+  List<Object> get props => [];
+}
+
+class ScheduleTransitionState extends ScheduleState{
+  final bool isShuttle;
+  final ScheduleState currentState;
+  const ScheduleTransitionState({@required this.isShuttle,@required this.currentState});
   @override
   List<Object> get props => [];
 }
