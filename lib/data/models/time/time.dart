ef1e817e63e5f368417d53f29f84e0efa48ffb01
unknown <alexboyuchen@gmail.com>
2020-10-30 15:37:48 -0700

    fixed class name conflict with Time from other modules

--- lib/data/models/time/time.dart
+++ lib/data/models/time/time.dart
@@ -1,12 +1,13 @@
 import 'package:equatable/equatable.dart';
 
-class Time extends Comparable<Time> {
+// ignore: camel_case_types
+class time extends Comparable<time> {
   bool isMilitary; //if time is military time (default is true)
   int hour;
   int minute;
   int second;
   String str;
-  Time({this.str, this.isMilitary = true}) {
+  time({this.str, this.isMilitary = true}) {
     // default constructor
     int first = str.indexOf(":");
     int second = str.lastIndexOf(":");
@@ -15,27 +16,28 @@ class Time extends Comparable<Time> {
     this.second = int.parse(str.substring(second + 1));
   }
 
-  Time.copyContructor({this.hour, this.minute, this.second, this.isMilitary}) {
-    this.str = null;
+  time.copyContructor({this.hour, this.minute, this.second, this.isMilitary}) {
+    this.str = 
+    "${this.hour.toString().padLeft(2,"0")}:${this.minute.toString().padLeft(2,"0")}:${this.second.toString().padLeft(2,"0")}";
   }
 
   @override
-  int compareTo(Time other) {
+  int compareTo(time other) {
     // return positive if current is larger than other can be used with .sort
     return (this.hour * 3600 + this.minute * 60 + this.second) -
         (other.hour * 3600 + other.minute * 60 + other.second);
   }
 
-  Time operator +(Time other) {
-    return Time.copyContructor(
+  time operator +(time other) {
+    return time.copyContructor(
         hour: this.hour + other.hour,
         minute: this.minute + other.minute,
         second: this.second + other.second,
         isMilitary: this.isMilitary);
   }
 
-  Time operator -(Time other) {
-    return Time.copyContructor(
+  time operator -(time other) {
+    return time.copyContructor(
         hour: this.hour - other.hour,
         minute: this.minute - other.minute,
         second: this.second - other.second,
