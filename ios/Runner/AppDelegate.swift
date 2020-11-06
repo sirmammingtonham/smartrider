b663df3529a46fd5a12760933ee67b261cf69aa4
Alex Montes <montea6@rpi.edu>
2020-11-06 18:31:19 -0500

    Added App Traqnsport Security Settings in plist for iOS Previously the application would build on iOS but would never load the map since iOS wouldn't allow the insecure bus tracking HTTP to connect–– this issue has been resolved

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,9 +15,6 @@ import GoogleMaps
         
     GMSServices.provideAPIKey(mapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
