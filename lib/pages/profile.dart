4bb6b7b070a36b3c8de7567df6163431ae032ab7
Megan Goulet <megan.elizabeth.goulet@gmail.com>
2020-11-15 01:18:22 -0500

    Modify Layout
    Modified the layout to match the theme discussed on Discord.
    
    + Still need to fix the color scheme and behaviors. This serves as a basic layout as a starting point for building the profile page.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,4 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:smartrider/data/models/themes.dart';
 import 'package:smartrider/blocs/authentication/authentication_bloc.dart';
@@ -44,18 +45,64 @@ class _ProfilePageState extends State<ProfilePage> {
                       Navigator.pop(context);
                     },
                     shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0))),
+                new Padding(
+                  /// WARNING: padding should be fixed to adjust to screen width.
+                  padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                ), // adds space between button and lower bezel
+                RaisedButton(
+                    child: Text(
+                      'SIGN OUT',
+                      style: Theme.of(context).textTheme.button,
+                    ),
+                    onPressed: () {
+                      BlocProvider.of<AuthenticationBloc>(context).add(
+                        AuthenticationLoggedOut(),
+                      );
+                      Navigator.pop(context);
+                    },
+                    shape: RoundedRectangleBorder(
                         borderRadius: new BorderRadius.circular(10.0)))
               ]),
+              new Padding(
+                /// WARNING: padding should be fixed to adjust to screen width.
+                padding: const EdgeInsets.symmetric(vertical: 40.0),
+              ),
 
-              Container(
-                color: Theme.of(context).buttonColor,
-                width: double.infinity,
-                height: 175.0,
-                child: Center(
-                    child: Text(
-                  'Hello, ' + widget.role,
-                  style: TextStyle(color: Colors.white70, fontSize: 50),
-                )),
+              /// FORMED TOP OF SCREEN
+              new Stack(
+                children: <Widget>[
+                  Container(
+                      color: Theme.of(context).buttonColor,
+                      width: double.infinity,
+                      height: 175.0,
+                      child: Column(
+                        children: [
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 45.0)),
+                          Center(
+                              child: Text(
+                            'Hello, ' + widget.role,
+                            style:
+                                TextStyle(color: Colors.grey[50], fontSize: 50),
+                          )),
+                        ],
+                      )),
+                  new Positioned(
+                    left: 120.0,
+                    bottom: 100,
+                    child: CircularProfileAvatar(
+                      'https://avatars0.githubusercontent.com/u/8264639?s=460&v=4',
+                      child: FlutterLogo(),
+                      borderColor: Colors.purpleAccent,
+                      borderWidth: 5,
+                      elevation: 2,
+                      radius: 70,
+                    ),
+                  ),
+                ],
+                overflow: Overflow.visible,
               ),
 
               Expanded(
@@ -64,41 +111,71 @@ class _ProfilePageState extends State<ProfilePage> {
                   child: ListView(
                     children: <Widget>[
                       ListTile(
-                        title: Text("Status"),
-                        subtitle: Text('online'),
-                        leading: Icon(Icons.person),
-                      ),
-                      ListTile(
                           title: Text("Role"),
                           subtitle: Text(widget.role),
                           leading: this.determinerole(widget.role)
                               ? Icon(Icons.book)
                               : Icon(Icons.drive_eta)),
                       ListTile(
-                        title: Text("email"),
+                        title: Text("Email"),
                         leading: Icon(Icons.email),
                         subtitle: Text(widget.email),
                       )
                     ],
+                    padding: const EdgeInsets.symmetric(vertical: 0.0),
                   ),
                 ),
               ),
-              RaisedButton(
-                  child: Text(
-                    'SIGN OUT',
-                    style: Theme.of(context).textTheme.button,
-                  ),
-                  onPressed: () {
-                    BlocProvider.of<AuthenticationBloc>(context).add(
-                      AuthenticationLoggedOut(),
-                    );
-                    Navigator.pop(context);
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(20.0))),
               new Padding(
                 padding: const EdgeInsets.symmetric(vertical: 10.0),
               ), // adds space between button and lower bezel
+              RaisedButton(
+                  padding: const EdgeInsets.symmetric(horizontal: 95.0),
+                  child: Text(
+                    'CHANGE PASSWORD',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0))),
+              RaisedButton(
+                  padding: const EdgeInsets.symmetric(horizontal: 119.0),
+                  child: Text(
+                    'REPORT BUG',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0))),
+              RaisedButton(
+                  padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                  child: Text(
+                    'REQUEST FEATURE',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0))),
+              RaisedButton(
+                  padding: const EdgeInsets.symmetric(horizontal: 102.0),
+                  child: Text(
+                    'DELETE ACCOUNT',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0))),
+              new Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+              ),
             ],
           ),
         ),
