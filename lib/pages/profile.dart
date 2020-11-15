b3ea7bc0c5c77a0e18ada15c2a5400c9731fa5c5
Megan Goulet <megan.elizabeth.goulet@gmail.com>
2020-11-15 03:47:04 -0500

    Add Documentation to Profile Page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,6 +1,5 @@
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/material.dart';
-import 'package:smartrider/data/models/themes.dart';
 import 'package:smartrider/blocs/authentication/authentication_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,10 +9,11 @@ abstract class ListItem {
   Widget buildSubtitle(BuildContext context);
 }
 
+/// Represents the physical profile page.
 class ProfilePage extends StatefulWidget {
-  final String role;
-  final String email;
-  final String name;
+  final String role; // Decides what role the user has (student, etc.)
+  final String email; // The user's email that is linked to their account.
+  final String name; // Name of the user.
   ProfilePage(
       {this.title,
       @required this.name,
@@ -21,6 +21,7 @@ class ProfilePage extends StatefulWidget {
       @required this.email});
   final String title;
 
+  /// Sets the state of the profile page.
   @override
   _ProfilePageState createState() => new _ProfilePageState();
 }
@@ -36,6 +37,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 padding: const EdgeInsets.symmetric(vertical: 20.0),
               ), // adds space between button and lower bezel
               Row(children: <Widget>[
+                // Back button
                 RaisedButton(
                     child: Text(
                       '< BACK',
@@ -46,10 +48,13 @@ class _ProfilePageState extends State<ProfilePage> {
                     },
                     shape: RoundedRectangleBorder(
                         borderRadius: new BorderRadius.circular(10.0))),
+
+                // Adds space between BACK button and SIGN OUT button.
                 new Padding(
                   /// WARNING: padding should be fixed to adjust to screen width.
                   padding: const EdgeInsets.symmetric(horizontal: 100.0),
-                ), // adds space between button and lower bezel
+                ),
+                // Sign Out button
                 RaisedButton(
                     child: Text(
                       'SIGN OUT',
@@ -64,23 +69,27 @@ class _ProfilePageState extends State<ProfilePage> {
                     shape: RoundedRectangleBorder(
                         borderRadius: new BorderRadius.circular(10.0)))
               ]),
+              // Space between row buttons and profile header
               new Padding(
                 /// WARNING: padding should be fixed to adjust to screen width.
                 padding: const EdgeInsets.symmetric(vertical: 40.0),
               ),
 
-              /// FORMED TOP OF SCREEN
+              // Controls overflow between profile pic and container.
               new Stack(
                 children: <Widget>[
+                  // Profile Header
                   Container(
                       color: Theme.of(context).buttonColor,
                       width: double.infinity,
                       height: 175.0,
                       child: Column(
                         children: [
+                          // Spacing below text
                           Padding(
                               padding:
                                   const EdgeInsets.symmetric(vertical: 45.0)),
+                          // Profile greeting text
                           Center(
                               child: Text(
                             'Hello, ' + widget.role,
@@ -89,10 +98,14 @@ class _ProfilePageState extends State<ProfilePage> {
                           )),
                         ],
                       )),
+                  // Controls where the profile picture is compare to the profile
+                  // greeting.
                   new Positioned(
                     left: 120.0,
                     bottom: 100,
+                    // Profile picture.
                     child: CircularProfileAvatar(
+                      // Link to profile pic:
                       'https://avatars0.githubusercontent.com/u/8264639?s=460&v=4',
                       child: FlutterLogo(),
                       borderColor: Colors.purpleAccent,
@@ -102,33 +115,41 @@ class _ProfilePageState extends State<ProfilePage> {
                     ),
                   ),
                 ],
+                // Profile pic overflows Stack. Keeps top portion of profile pic
+                // visible to user.
                 overflow: Overflow.visible,
               ),
-
+              // List of attributes on the user's profile
               Expanded(
                 child: SizedBox(
-                  height: 150,
+                  height: 180,
                   child: ListView(
+                    // Keeps list from scrolling (if more attributes are added,
+                    // we should make the profile page scrollable and reveal
+                    // the action buttons as the user scrolls down)
+                    padding: const EdgeInsets.symmetric(vertical: 0.0),
                     children: <Widget>[
+                      // Describes the role
                       ListTile(
                           title: Text("Role"),
                           subtitle: Text(widget.role),
                           leading: this.determinerole(widget.role)
                               ? Icon(Icons.book)
                               : Icon(Icons.drive_eta)),
+                      // Describes the user's email
                       ListTile(
                         title: Text("Email"),
                         leading: Icon(Icons.email),
                         subtitle: Text(widget.email),
                       )
                     ],
-                    padding: const EdgeInsets.symmetric(vertical: 0.0),
                   ),
                 ),
               ),
               new Padding(
                 padding: const EdgeInsets.symmetric(vertical: 10.0),
-              ), // adds space between button and lower bezel
+              ), // adds space between attributes and action buttons
+              // Change Password Button
               RaisedButton(
                   padding: const EdgeInsets.symmetric(horizontal: 95.0),
                   child: Text(
@@ -136,10 +157,12 @@ class _ProfilePageState extends State<ProfilePage> {
                     style: Theme.of(context).textTheme.button,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    // TO DO: What to do when the user wants to change their
+                    // password?
                   },
                   shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(10.0))),
+              // Report Bug Button
               RaisedButton(
                   padding: const EdgeInsets.symmetric(horizontal: 119.0),
                   child: Text(
@@ -147,10 +170,11 @@ class _ProfilePageState extends State<ProfilePage> {
                     style: Theme.of(context).textTheme.button,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    // TO DO: What to do when the user wants to report a bug?
                   },
                   shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(10.0))),
+              // Request Feature Button
               RaisedButton(
                   padding: const EdgeInsets.symmetric(horizontal: 100.0),
                   child: Text(
@@ -158,10 +182,12 @@ class _ProfilePageState extends State<ProfilePage> {
                     style: Theme.of(context).textTheme.button,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    // TO DO: What to do when the user wants to request a
+                    // feature for this app?
                   },
                   shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(10.0))),
+              // Delete Account Button
               RaisedButton(
                   padding: const EdgeInsets.symmetric(horizontal: 102.0),
                   child: Text(
@@ -169,10 +195,12 @@ class _ProfilePageState extends State<ProfilePage> {
                     style: Theme.of(context).textTheme.button,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    // TO DO: What to do when the user wants to delete their
+                    // account?
                   },
                   shape: RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(10.0))),
+              // Adds space between action buttons and bottom of screen.
               new Padding(
                 padding: const EdgeInsets.symmetric(vertical: 20.0),
               ),
@@ -182,6 +210,7 @@ class _ProfilePageState extends State<ProfilePage> {
         theme: Theme.of(context));
   }
 
+  /// Determines the assigned role that the user has.
   determinerole(String r) {
     if (r[0] == 'S') {
       return true;
