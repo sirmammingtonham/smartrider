419c09d5922a62d4b51424a7c4094824191cf71d
sirmammingtonham <ethanj217@gmail.com>
2021-03-01 14:30:51 -0800

    add basic safe ride state to search bar

---
+++ lib/util/multi_bloc_builder.dart
@@ -0,0 +1,110 @@
+// https://github.com/Usorsoft/flutter_multi_bloc_builder
+
+library flutter_multi_bloc_builder;
+
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+class MultiBlocBuilder extends StatefulWidget {
+  final Widget Function(BuildContext, BlocStates) _builder;
+  final List<Bloc> _blocs;
+
+  /// [MultiBlocBuilder] handles building a widget by observig state of variouse [Bloc]s 
+  /// and should be used in combination with the [flutter_bloc](https://pub.dev/packages/flutter_bloc) package.
+  /// Specify the bloc to observed via the [blocs] parameter.
+  /// The [builder] rebuilds each time a state change occurs and provides a context ([BuildContext]) and the states ([BlocStates]).
+  /// 
+  /// How to use:
+  /// ```
+  /// final bloc1 = BlocProvider.of<MyBloc1>(context);
+  /// final bloc2 = BlocProvider.of<MyBloc2>(context);
+  /// final bloc3 = BlocProvider.of<MyBloc2>(context);
+  /// 
+  /// MultiBlocBuilder(
+  ///   blocs: [bloc1, bloc2, bloc3],
+  ///   builder: (context, states) {
+  ///     final state1 = states.get<MyBloc1State>();
+  ///     final state2 = states.get<MyBloc2State>();
+  ///     final state3 = states.get<MyBloc3State>();
+  ///     
+  ///     if (state1 is Loading || state2 is Loading || state3 is Loading) {
+  ///       return Text("Loading");
+  ///     } else {
+  ///       return Text("SHow some content");
+  ///     }
+  ///   }
+  /// );
+  /// ```
+  MultiBlocBuilder({
+    @required List<Bloc> blocs, 
+    @required Widget Function(BuildContext, BlocStates) builder
+  }) :  assert(blocs != null), 
+        assert(builder != null),
+        _blocs = blocs,
+        _builder = builder;
+
+  @override
+  State<StatefulWidget> createState() => _MultiBlocState();
+}
+
+/// Serves as the state of the [MultiBlocBuilder].
+class _MultiBlocState extends State<MultiBlocBuilder> {
+  final _stateSubscriptions = List<StreamSubscription>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget._blocs.forEach( (bloc) {
+      final subscription = bloc.listen((state) => setState(() {}));
+      _stateSubscriptions.add(subscription);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final states = widget._blocs.map( (bloc) => bloc.state).toList();
+    return widget._builder(context, BlocStates._private(states));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stateSubscriptions.forEach( (subscription) {
+      subscription.cancel();
+    });
+  }
+}
+
+/// [BlocStates] serves as a container of [dynamic] objects (usually bloc states). 
+/// It is part of the [MultiBlocProvider] package and therefore mainly used
+/// to provide a varety of bloc states via the [MultiBlocBuilder._builder] function.
+/// 
+/// A bloc states can be retrieved via the [get] method and the wanted type like this:
+/// ```
+/// final exampeState = blocStates.get<ExampleState>();
+/// ```
+///
+class BlocStates {
+  final List _stateContainer = List<dynamic>();
+
+  // Private contructor 
+  BlocStates._private(List states) : assert(states != null) {
+    if (states != null) {
+      _stateContainer.addAll(states);
+    }
+  }
+
+  /// Retrieves the first object that matches the generic or null if no machting object available.
+  /// __NOTE:__ Please ensure that [BlocStates] doesn't contain multiple objects of the same type.
+  /// 
+  /// How to use:
+  /// ```
+  /// final exampeState = blocStates.get<ExampleState>();
+  /// ```
+  T get<T>() => _stateContainer.firstWhere( 
+    (entry) => (entry is T),
+    orElse: null
+  );
+}
--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:smartrider/blocs/map/map_bloc.dart';
+import 'package:smartrider/util/multi_bloc_builder.dart';
+
 import 'package:smartrider/pages/profile.dart';
 import 'package:smartrider/widgets/icons.dart';
 import 'dart:io' show Platform;
@@ -21,6 +23,8 @@ import 'package:smartrider/widgets/destination_autocomplete.dart';
 // import 'dart:io';
 import 'package:smartrider/util/strings.dart';
 
+const testCoord = LatLng(42.729280, -73.679056);
+
 String computeUsername(String name) {
   //compute initials to be displayed on search bar
   var counter = 1;
@@ -37,9 +41,12 @@ class SearchBar extends StatefulWidget {
 }
 
 class SearchBarState extends State<SearchBar> {
+  double
+      topBarDist; // Distance between top of phone bezel & top search bar //TODO: use fraction instead of hard coded value
   String name;
   String role;
   MapBloc mapBloc;
+  AuthenticationBloc authBloc;
   final geocoder = GoogleMapsGeocoding(apiKey: GOOGLE_API_KEY);
 
   SearchBarState();
@@ -49,11 +56,19 @@ class SearchBarState extends State<SearchBar> {
     super.initState();
 
     mapBloc = BlocProvider.of<MapBloc>(context);
+    authBloc = BlocProvider.of<AuthenticationBloc>(context);
+
+    if (Platform.isAndroid) {
+      topBarDist = 30.0;
+    } else {
+      topBarDist = 45.0;
+    }
   }
 
   @override
   void dispose() {
     mapBloc.close();
+    authBloc.close();
     super.dispose();
   }
 
@@ -76,90 +91,126 @@ class SearchBarState extends State<SearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    var topBarDist; //Distance between top of phone bezel & top search bar
-    if (Platform.isAndroid) {
-      topBarDist = 30.0;
-    } else {
-      topBarDist = 45.0;
-    }
-    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
-      builder: (context, state) {
-        if (state is AuthenticationSuccess) {
-          name = state.displayName;
-          role = state.role;
+    return MultiBlocBuilder(
+      blocs: [mapBloc, authBloc],
+      builder: (context, states) {
+        final mapState = states.get<MapState>();
+        final authState = states.get<AuthenticationState>();
+
+        name = authState is AuthenticationSuccess ? authState.displayName : '';
+        role = authState is AuthenticationSuccess ? authState.role : '';
+        
+        if (mapState is MapLoadedState && mapState.mapState == MapStateEnum.saferide) {
           return Positioned(
-            top: topBarDist,
-            right: 15,
-            left: 15,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              height: 55,
-              child: Material(
-                borderRadius: BorderRadius.circular(10.0),
-                elevation: 6.0,
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(SR_Icons.Settings),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          // BlocProvider.of<PrefsBloc>(context).add(LoadPrefsEvent());
-                          return SettingsPage();
-                        }));
-                      },
-                    ),
-                    Expanded(
-                        // creates the autocomplete field (requires strings.dart in the utils folder to contain the api key)
-                        child: PlacesAutocompleteField(
-                      apiKey:
-                          GOOGLE_API_KEY, //Platform.environment['MAPS_API_KEY'],
-                      hint: "Need a Safe Ride?",
-                      location: Location(
-                          42.729980, -73.676682), // location of union as center
-                      radius:
-                          1000, // 1km from union seems to be a good estimate of the bounds on safe ride's website
-                      language: "en",
-                      components: [Component(Component.country, "us")],
-                      strictbounds: true,
-                      // sessionToken: Uuid().generateV4(),
-                      inputDecoration: null,
-                      onSelected: onAutocompleteSelect,
-                    )),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).buttonColor,
-                        child: IconButton(
-                          icon: Text(computeUsername(name),
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white70)),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ProfilePage(
-                                          title: computeUsername(name),
-                                          name: null,
-                                          role: role,
-                                          email: name,
-                                        )));
-                          },
+              top: topBarDist,
+              right: 15,
+              left: 15,
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Card(
+                      elevation: 6.0,
+                      child: Column(mainAxisSize: MainAxisSize.max, children: [
+                        ListTile(
+                          leading: Container(
+                            height: double.infinity,
+                            child: Icon(Icons.my_location),
+                          ),
+                          title: const Text('Card title 1'),
+                          subtitle: const Text('Pickup location'),
+                          onTap: () {},
                         ),
-                        //Text('JS', style: TextStyle(color: Colors.white70)),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+                        Divider(height: 0),
+                        ListTile(
+                          leading: Container(
+                              height: double.infinity,
+                              child: Icon(Icons.place)),
+                          title: const Text('Card title 2'),
+                          subtitle: const Text('Dropoff location'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.cancel),
+                            onPressed: () {
+                              print('pressed');
+                            },
+                          ),
+                          onTap: () {},
+                        )
+                      ]))));
         } else {
-          print("something's wrong with auth bloc");
-          return Positioned(
-              child: Container(child: CircularProgressIndicator()));
+          // print("something's wrong with auth bloc");
+          return searchBar();
         }
       },
     );
   }
+
+  Widget searchBar() => Positioned(
+        top: topBarDist,
+        right: 15,
+        left: 15,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          height: 55,
+          child: Material(
+            borderRadius: BorderRadius.circular(10.0),
+            elevation: 6.0,
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(SR_Icons.Settings),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      // BlocProvider.of<PrefsBloc>(context).add(LoadPrefsEvent());
+                      return SettingsPage();
+                    }));
+                  },
+                ),
+                Expanded(
+                    child: ListTile(
+                        title: const Text('Test saferide call to union'),
+                        onTap: () => mapBloc
+                            .add(MapSaferideCalledEvent(coord: testCoord)))),
+                // creates the autocomplete field (requires strings.dart in the utils folder to contain the api key)
+                //     PlacesAutocompleteField(
+                //   apiKey:
+                //       GOOGLE_API_KEY, //Platform.environment['MAPS_API_KEY'],
+                //   hint: "Need a Safe Ride?",
+                //   location: Location(
+                //       42.729980, -73.676682), // location of union as center
+                //   radius:
+                //       1000, // 1km from union seems to be a good estimate of the bounds on safe ride's website
+                //   language: "en",
+                //   components: [Component(Component.country, "us")],
+                //   strictbounds: true,
+                //   inputDecoration: null,
+                //   onSelected: onAutocompleteSelect,
+                // )),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: CircleAvatar(
+                    backgroundColor: Theme.of(context).buttonColor,
+                    child: IconButton(
+                      icon: Text(computeUsername(name),
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.white70)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfilePage(
+                                      title: computeUsername(name),
+                                      name: null,
+                                      role: role,
+                                      email: name,
+                                    )));
+                      },
+                    ),
+                    //Text('JS', style: TextStyle(color: Colors.white70)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 }
