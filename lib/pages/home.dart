221aac7292412613ad1ce378236b32bd6254bf34
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-03-12 18:38:43 -0500

    Prevented showcase to repeat
    Made it so that the showcase would not happen if the UI is built up a second time.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,6 +25,7 @@ GlobalKey showcaseSlidingPanel = GlobalKey();
 GlobalKey showcaseViewChange = GlobalKey();
 GlobalKey showcaseLocation = GlobalKey();
 GlobalKey showcaseSearch = GlobalKey();
+var flag = false;
 
 /// Default page that is displayed once the user logs in.
 class HomePage extends StatelessWidget {
@@ -77,14 +78,17 @@ class _HomePageState extends State<_HomePage>
   /// Builds the map and the schedule dropdown based on dynamic data.
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => ShowCaseWidget.of(context).startShowCase([
-              showcaseSettings,
-              showcaseProfile,
-              showcaseViewChange,
-              showcaseLocation,
-              showcaseSlidingPanel
-            ]));
+    if (flag == false) {
+      WidgetsBinding.instance.addPostFrameCallback(
+          (_) => ShowCaseWidget.of(context).startShowCase([
+                showcaseSettings,
+                showcaseProfile,
+                showcaseViewChange,
+                showcaseLocation,
+                showcaseSlidingPanel
+              ]));
+      flag = true;
+    }
 
     /// Height of the stop schedules when open
     _panelHeightOpen = MediaQuery.of(context).size.height * .95;
