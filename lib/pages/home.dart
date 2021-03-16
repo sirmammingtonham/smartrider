369fcf36c4f6595b1bff7030b827aa6c6cd3ba2f
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-03-16 17:07:54 -0400

    Fixed rework

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -79,6 +79,18 @@ class _HomePageState extends State<_HomePage>
   /// Builds the map and the schedule dropdown based on dynamic data.
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => ShowCaseWidget.of(context).startShowCase([
+              showcaseSettings,
+              showcaseProfile,
+              showcaseSlidingPanel,
+              showcaseViewChange,
+              showcaseLocation,
+              showcaseSearch,
+              showcaseBusTab,
+              showcaseMap
+            ]));
+
     /// Height of the stop schedules when open
     _panelHeightOpen = MediaQuery.of(context).size.height * .95;
     return Material(
