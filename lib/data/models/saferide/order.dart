6faeb89920c67f2b75f8bce67099b34c8839aa44
sirmammingtonham <ethanj217@gmail.com>
2021-03-16 14:46:46 -0700

    add saferide models

---
+++ lib/data/models/saferide/order.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:smartrider/data/models/saferide/location_data.dart';
+import 'package:smartrider/data/models/backend/user.dart';
+
+class Order {
+  String id;
+  String status;
+  String tripId;
+  LocationData pickup;
+  LocationData dropoff;
+  User rider;
+  User driver;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  Order(
+      {this.id,
+      @required this.status,
+      this.tripId,
+      @required this.pickup,
+      @required this.dropoff,
+      @required this.rider,
+      this.driver,
+      this.createdAt,
+      this.updatedAt});
+
+  Order.fromDocument(doc) {
+    this.id = this.status = doc['status'];
+    this.tripId = doc['trip_id'];
+    this.pickup = LocationData.fromDocument(doc['pickup']);
+    this.dropoff = doc['dropoff'];
+  }
+}
