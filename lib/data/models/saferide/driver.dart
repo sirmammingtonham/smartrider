1a6afda235c0d792822e679376659540686eaa9d
deanvogel <dnv185@hotmail.com>
2021-03-19 18:21:38 -0400

    added driver.dart, updated saferide provider

---
+++ lib/data/models/saferide/driver.dart
@@ -0,0 +1,25 @@
+class Driver {
+  String deviceId;
+  String name;
+  String email;
+  String phone;
+  bool available;
+  String licensePlate;
+
+  Driver(
+      {this.deviceId,
+      this.name,
+      this.email,
+      this.phone,
+      this.available,
+      this.licensePlate});
+
+  Driver.fromDocument(doc) {
+    this.deviceId = doc['device_id'];
+    this.name = doc['name'];
+    this.email = doc['email'];
+    this.phone = doc['phone'];
+    this.available = doc['available'];
+    this.licensePlate = doc['license_plate'];
+  }
+}
--- lib/data/providers/saferide_provider.dart
+++ lib/data/providers/saferide_provider.dart
@@ -3,9 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // saferide imports
 import 'package:hypertrack_views_flutter/hypertrack_views_flutter.dart';
 import 'package:smartrider/util/strings.dart';
-
 // saferide models
 import '../models/saferide/order.dart';
+import '../models/saferide/driver.dart';
 
 class SaferideProvider {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -26,6 +26,16 @@ class SaferideProvider {
       'createdAt': order.createdAt,
       'updatedAt': order.updatedAt
     });
+  }
+
+  Future<Map<String, Driver>> getDrivers() async {
+    QuerySnapshot response = await firestore.collection('drivers').get();
+
+    Map<String, Driver> driversMap = Map.fromIterable(response.docs,
+        key: (doc) => doc['device_id'],
+        value: (doc) => Driver.fromDocument(doc.data()));
+
+    return driversMap;
   }
 
   Stream<MovementStatus> subscribeToDeviceUpdates(String deviceId) =>
