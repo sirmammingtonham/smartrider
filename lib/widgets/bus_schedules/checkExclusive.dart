10898b3d7dc157597e125b9140da8c665977d74d
unknown <alexboyuchen@gmail.com>
2021-04-27 22:39:09 -0700

    oops used the wrong format

--- lib/widgets/bus_schedules/checkExclusive.dart
+++ lib/widgets/bus_schedules/checkExclusive.dart
@@ -3,7 +3,7 @@ import 'package:smartrider/data/models/bus/bus_timetable.dart';
 
 bool checkExclusiveDates(BusTimetable timetable) {
   final DateTime now = DateTime.now();
-  final DateFormat formatter = DateFormat('yyyyMMdd');
+  final DateFormat formatter = DateFormat.yMMMMd('en_US');
   final String today = formatter.format(now);
   if (timetable.excludeDates.contains(today)) {
     return true;
