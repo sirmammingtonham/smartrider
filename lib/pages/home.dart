e110e3f3ff504d7934e0a846ae197af2ec07e55b
sirmammingtonham <ethanj217@gmail.com>
2021-06-07 17:21:57 -0400

    start adding files we need

---
+++ lib/pages/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,2 @@
+// we should have an account associated with each saferide car 
+// that they need to use to log in
