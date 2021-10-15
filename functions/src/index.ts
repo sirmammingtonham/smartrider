fc1352629a105a8c4127fe610619b27162f007f0
deanvogel <dnv185@hotmail.com>
2021-10-15 17:16:18 -0400

    fixed functions folder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,41 +1,40 @@
 import * as admin from 'firebase-admin';
-admin.initializeApp(); // needs to go before other imports: https://github.com/firebase/firebase-functions-test/issues/6#issuecomment-496021884
 import * as functions from "firebase-functions";
-/// bus functions
-import * as db from './bus/update_firestore_gtfs';
-export const refreshDataBase = db.refreshDataBase;
-import * as sfcu from './saferide/saferide_cleanup';
-export const saferideCleanup = sfcu.saferideOrderFlush;
 import * as cors_proxy from 'cors-anywhere';
+admin.initializeApp(); // needs to go before other imports: https://github.com/firebase/firebase-functions-test/issues/6#issuecomment-496021884
 
 const runtimeOpts: functions.RuntimeOptions = {
-    timeoutSeconds: 3, // timeout function after 2 secs
-    memory: "256MB", // allocate 256MB of mem per function
+    timeoutSeconds: 3, // timeout function after 3 secs
+    memory: "128MB", // allocate 128MB of mem per function
 };
 
-/// saferide functions
-// import * as saferide from "./saferide/saferide_listeners";
-// import * as saferide_webhooks from "./saferide/saferide_webhooks";
-// export const saferideOnOrderUpdate = saferide.onOrderUpdate;
-// export const saferideOnDriverUpdate = saferide.onDriverUpdate;
-
-// export const hypertrackTripWebhook = saferide_webhooks.hypertrackTripWebhook;
-
-/// test functions
-// import * as db_tests from './bus/bus_test_funcs';
-import * as saferide_tests from "./saferide/saferide_test_funcs";
-export const createTest = saferide_tests.createTest;
-export const addTestDriver = saferide_tests.addTestDriver;
-// export const testDB = db_tests.testDB;
+const firestore = admin.firestore();
 const proxy = cors_proxy.createServer({
-    // requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: [
         'cookie',
         'cookie2',
     ],
 });
 
-export const proxyWithCorsAnywhere = functions
+export { refreshDataBase } from './update_firestore_gtfs';
+
+// Clears the firestore database of any orders that have not errored
+// Runs at 3:00 am everyday eastern time automatically
+export const saferideOrderFlush = functions
+    .runWith(runtimeOpts)
+    .pubsub.schedule("0 3 * * *")
+    .timeZone("America/New_York")
+    .onRun(async (_context) => {
+        const snapshot = await firestore.collection("orders").get();
+        for (const doc of snapshot.docs) {
+            if (doc.data().status !== "ERROR") {
+                await doc.ref.delete();
+            }
+        }
+    })
+
+// Allows bypassing of cors restrictions using a proxy server
+export const corsProxy = functions
     .runWith(runtimeOpts)
     .https.onRequest((req, res) => {
     proxy.emit('request', req, res);
